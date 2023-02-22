06fdf0850343c801a31cc9b2b6b03ef39fe36678
Jacek Grzybowski <totospl99@gmail.com>
2023-02-22 22:18:32 +0100

    mpu6050 readout

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <TinyGPSPlus.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
+#include <Adafruit_MPU6050.h>
 #include <Adafruit_HMC5883_U.h>
 
 #define GPS_SERIAL_RX 16
@@ -10,10 +11,11 @@
 #define GPS_SERIAL_BAUD_RATE 38400
 #define GPS_SERIAL_MODE SERIAL_8N1
 
-
+#define MPU_ADRESS (u_int8_t)0x69
 
 TinyGPSPlus gps;
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+Adafruit_MPU6050 mpu;
 
 void displayInfo()
 {
@@ -21,46 +23,8 @@ void displayInfo()
   if (gps.location.isValid())
   {
     Serial.print(gps.location.lat(), 6);
-    Serial.print(F(","));
+    Serial.print(F(", "));
     Serial.print(gps.location.lng(), 6);
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial.print(F("  Date/Time: "));
-  if (gps.date.isValid())
-  {
-    Serial.print(gps.date.month());
-    Serial.print(F("/"));
-    Serial.print(gps.date.day());
-    Serial.print(F("/"));
-    Serial.print(gps.date.year());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial.print(F(" "));
-  if (gps.time.isValid())
-  {
-    if (gps.time.hour() < 10)
-      Serial.print(F("0"));
-    Serial.print(gps.time.hour());
-    Serial.print(F(":"));
-    if (gps.time.minute() < 10)
-      Serial.print(F("0"));
-    Serial.print(gps.time.minute());
-    Serial.print(F(":"));
-    if (gps.time.second() < 10)
-      Serial.print(F("0"));
-    Serial.print(gps.time.second());
-    Serial.print(F("."));
-    if (gps.time.centisecond() < 10)
-      Serial.print(F("0"));
-    Serial.print(gps.time.centisecond());
   }
   else
   {
@@ -78,12 +42,41 @@ void displayInfo()
     Serial.print(F("INVALID"));
   }
 
-  sensors_event_t mag_event; 
+  sensors_event_t mag_event;
   mag.getEvent(&mag_event);
   {
-    Serial.print(" Compass X: "); Serial.print(mag_event.magnetic.x);
-    Serial.print(" Y: "); Serial.print(mag_event.magnetic.y);
-    Serial.print(" Z: "); Serial.print(mag_event.magnetic.z);
+    Serial.print(" Compass X: ");
+    Serial.print(mag_event.magnetic.x);
+    Serial.print(" Y: ");
+    Serial.print(mag_event.magnetic.y);
+    Serial.print(" Z: ");
+    Serial.print(mag_event.magnetic.z);
+  }
+
+  {
+    /* Get new sensor events with the readings */
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+
+    /* Print out the values */
+    Serial.print(" ");
+    Serial.print("AccelX:");
+    Serial.print(a.acceleration.x);
+    Serial.print(",");
+    Serial.print("AccelY:");
+    Serial.print(a.acceleration.y);
+    Serial.print(",");
+    Serial.print("AccelZ:");
+    Serial.print(a.acceleration.z);
+    Serial.print(", ");
+    Serial.print("GyroX:");
+    Serial.print(g.gyro.x);
+    Serial.print(",");
+    Serial.print("GyroY:");
+    Serial.print(g.gyro.y);
+    Serial.print(",");
+    Serial.print("GyroZ:");
+    Serial.print(g.gyro.z);
   }
 
   Serial.println();
@@ -94,17 +87,34 @@ void setup()
   Serial.begin(9600);
   Serial2.begin(GPS_SERIAL_BAUD_RATE, GPS_SERIAL_MODE, GPS_SERIAL_RX, GPS_SERIAL_TX);
 
-  if(!mag.begin())
+  if (!mag.begin())
   {
-    /* There was a problem detecting the HMC5883 ... check your connections */
-    Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
-    while(1);
+    while (1)
+    {
+      Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
+      delay(500);
+    }
   }
+
+  if (!mpu.begin(MPU_ADRESS))
+  {
+    while (1)
+    {
+      Serial.println("Failed to find MPU6050 chip");
+      delay(500);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  // setupt motion detection
+  mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
+  mpu.setGyroRange(MPU6050_RANGE_1000_DEG);
 }
 
 void loop()
 {
-  while (Serial2.available() > 0){
+  while (Serial2.available() > 0)
+  {
     if (gps.encode(Serial2.read()))
       displayInfo();
   }
@@ -114,4 +124,3 @@ void loop()
     Serial.println(F("No GPS detected: check wiring."));
   }
 }
-
