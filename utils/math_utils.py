507c9c0c19a5025da8c0f72a61eb57a97327aa2a
Jacek Grzybowski <totospl99@gmail.com>
2023-08-02 02:54:30 +0200

    quarterions

--- utils/math_utils.py
+++ utils/math_utils.py
@@ -30,15 +30,15 @@ class ComplimentaryFilter:
 
 
 def get_rot_mat(roll, pitch, yaw):
-    a = yaw
+    a = roll
     b = pitch
-    y = roll
+    y = yaw
     return np.array([
-        [cos(a)*cos(b), cos(a)*sin(b)*sin(y)-sin(a) *
-         cos(y), cos(a)*sin(b)*cos(y)+sin(a)*sin(y)],
-        [sin(a)*cos(b), sin(a)*sin(b)*sin(y)+cos(a) *
-         cos(y), sin(a)*sin(b)*cos(y)-cos(a)*sin(y)],
-        [-sin(b), cos(b)*sin(y), cos(b)*cos(y)]
+        [cos(b)*cos(y), sin(a)*sin(b)*cos(y)-cos(a) *
+         sin(y), cos(a)*sin(b)*cos(y)+sin(a)*sin(y)],
+        [cos(b)*sin(y), sin(a)*sin(b)*sin(y)+cos(a) *
+         cos(y), cos(a)*sin(b)*sin(y)-sin(a)*cos(y)],
+        [-sin(b), sin(a)*cos(b), cos(a)*cos(b)],
     ], dtype=np.float32)
 
 
@@ -57,33 +57,64 @@ def quat_inv(quat):
 
 def quat_mult(p, q):
     return np.array([
-        p[0]*q[0]-q[1]*q[1]-p[2]*q[2]-p[3]*q[3],
-        p[0]*q[1]+p[1]*q[0]+p[2]*q[3]-p[3]*q[2],
-        p[0]*q[2]-p[1]*q[3]+p[2]*q[0]+p[3]*q[1],
-        p[0]*q[3]+p[1]*q[2]-p[2]*q[1]+p[3]*q[0],
+        p[0]*q[0] - p[1]*q[1] - p[2]*q[2] - p[3]*q[3],
+        p[0]*q[1] + p[1]*q[0] + p[2]*q[3] - p[3]*q[2],
+        p[0]*q[2] - p[1]*q[3] + p[2]*q[0] + p[3]*q[1],
+        p[0]*q[3] + p[1]*q[2] - p[2]*q[1] + p[3]*q[0],
     ])
 
 
-drone_transform = get_rot_mat(0, np.pi, -0.75*np.pi)
-eigenvalues, eigenvectors = np.linalg.eig(drone_transform)
+def rot_mat_to_quat(m):
+    if m[2, 2] < 0:
+        if m[0, 0] > m[1, 1]:
+            t = 1 + m[0, 0] - m[1, 1] - m[2, 2]
+            q = [t, m[0, 1]+m[1, 0], m[2, 0]+m[0, 2], m[1, 2]-m[2, 1]]
+        else:
+            t = 1 - m[0, 0] + m[1, 1] - m[2, 2]
+            q = [m[0, 1]+m[1, 0], t, m[1, 2] + m[2, 1], m[2, 0] - m[0, 2]]
+    else:
+        if m[0, 0] < -m[1, 1]:
+            t = 1 - m[0, 0] - m[1, 1] + m[2, 2]
+            q = [m[2, 0] + m[0, 2], m[1, 2] + m[2, 1], t, m[0, 1] - m[1, 0]]
+        else:
+            t = 1 + m[0, 0] + m[1, 1] + m[2, 2]
+            q = [m[1, 2]-m[2, 1], m[2, 0]-m[0, 2], m[0, 1]-m[1, 0], t]
+    q = np.array(q) * (0.5 / np.sqrt(t))
+    return q
 
-pointing_vec = (eigenvalues[0] * eigenvectors[0]) / \
-    np.linalg.norm(eigenvalues[0] * eigenvectors[0])
 
-theta = np.arccos(0.5 * (np.trace(drone_transform) - 1))
-# w x y z
-quat = np.array([
-    np.cos(theta / 2),
-    np.sin(theta / 2) * pointing_vec[0],
-    np.sin(theta / 2) * pointing_vec[1],
-    np.sin(theta / 2) * pointing_vec[2],
-])
+def rot_mat_to_quat_old(mat):
+    eigenvalues, eigenvectors = np.linalg.eig(mat)
+    target = 0
+    for i in range(1, 3):
+        if np.abs(eigenvalues[i] - 1) < np.abs(eigenvalues[target] - 1):
+            target = i
+    vec = eigenvalues[target] * eigenvectors[target]
+    vec = vec / np.linalg.norm(vec)
+    theta = np.arccos(0.5 * (np.trace(mat) - 1))
+    # w x y z
+    quat = np.array([
+        np.cos(theta / 2),
+        np.sin(theta / 2) * vec[0],
+        np.sin(theta / 2) * vec[1],
+        np.sin(theta / 2) * vec[2],
+    ])
+    return quat
 
-rot_vec1 = np.array([1, 0, 0])
-rot_q1 = np.array([0, 1, 0, 0])
 
-rot_vec2 = np.matmul(drone_transform, rot_vec1)
-rot_q2 = quat_mult(quat_mult(quat, rot_q1), quat_inv(quat))
+def quat_sandwich(q, v):
+    '''returns q*v*q_inv'''
+    return quat_mult(quat_mult(q, v), quat_inv(q))
 
-print(rot_vec2, rot_q2)
-print(quat_mult(rot_q1, quat_inv(rot_q1)))
+
+for i in np.arange(0, np.pi, 0.1):
+    drone_transform = get_rot_mat(i, i, -0.75*i)
+    q_rot = rot_mat_to_quat(drone_transform)
+
+    vec = np.array([1, 0, 0])
+    vec = np.matmul(drone_transform, vec)
+
+    quat = np.array([0, 1, 0, 0])
+    quat = quat_sandwich(q_rot, quat)[1::]
+
+    print(np.linalg.norm(vec - quat))
